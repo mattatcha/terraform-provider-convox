634e5cce0f6ccb3393243d2a253068fbd7d72843
Brendan Erwin <brendanjerwin@gmail.com>
2017-05-17 07:50:09 -0400

    building and the test suite is executing

--- convox/resource_convox_syslog_test.go
+++ convox/resource_convox_syslog_test.go
@@ -1,12 +1,12 @@
 package convox_test
 
 import (
-	. "github.com/terraform-provider-convox/convox"
-
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("ResourceConvoxSyslog", func() {
-
+	XIt("should fail", func() {
+		Expect(false).To(BeTrue())
+	})
 })
